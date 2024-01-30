81a45cf317310b58743d9a27e83a34fa5c00c259
Wilfried <64591090+la2spaille@users.noreply.github.com>
2024-01-30 18:46:56 +0100

    Feat : Header Yellow

---
+++ dotstarter/layouts/header-yellow/header-yellow.php
@@ -0,0 +1,22 @@
+<div class="f-header-yellow">
+    <div class="l-container">
+        <div class="f-header-yellow__tb">
+            <div class="f-header-yellow__wrapper">
+                <div class="f-header-yellow__title heading1">
+                    <h1>
+                        <?php the_sub_field('title') ?>
+                    </h1>
+                </div>
+                <div class="f-header-yellow__content">
+                    <h3 class="f-header-yellow__intro heading3">
+                        <?php the_sub_field('intro') ?>
+                    </h3>
+                    <p class="f-header-yellow__paragraph body-md">
+                        <?php the_sub_field('paragraph') ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
