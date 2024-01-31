16295b7a55bd0ef40481cf971790b6aa4bca58c0
Pierrick <pierrick@studio-dot.fr>
2024-01-31 15:40:22 +0100

    feat(newsletter block)

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,8 +1,12 @@
 import { registerGsapPlugins } from './config/gsap'
 import Menu from './components/menu'
+import Newsletter from './components/newsletter'
 
 /* eslint-disable */
 jQuery(function ($) {
+
+  alert('Hello from your JavaScript file!')
   registerGsapPlugins()
   Menu();
+  Newsletter();
 })
