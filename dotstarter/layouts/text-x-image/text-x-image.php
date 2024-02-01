f9ea8904fe9354715842c78284730df5433e643b
Wilfried <64591090+la2spaille@users.noreply.github.com>
2024-02-01 11:38:26 +0100

    Feat : Bg lines desktop

--- dotstarter/layouts/text-x-image/text-x-image.php
+++ dotstarter/layouts/text-x-image/text-x-image.php
@@ -1,7 +1,6 @@
 <div class="f-text-x-image">
     <div class="l-container">
         <div class="f-text-x-image__tb">
-
             <?php $i = 0; ?>
             <?php while (have_rows('texte_x_image')):
                 the_row() ?>
@@ -16,6 +15,16 @@
                     </div>
                 </div>
             <?php endwhile; ?>
+        </div>
+        <div class="f-text-x-image__bg">
+            <svg viewBox="0 0 1538 4930" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <line opacity="0.1" x1="0.5" y1="2.18557e-08" x2="0.499605" y2="9039" stroke="#979797"/>
+                <line opacity="0.1" x1="307.5" y1="2.18557e-08" x2="307.5" y2="9039" stroke="#979797"/>
+                <line opacity="0.1" x1="614.5" y1="2.68598e-08" x2="614.5" y2="9039" stroke="#979797"/>
+                <line opacity="0.1" x1="921.5" y1="2.68598e-08" x2="921.5" y2="9039" stroke="#979797"/>
+                <line opacity="0.1" x1="1228.5" y1="2.18557e-08" x2="1228.5" y2="9039" stroke="#979797"/>
+                <line opacity="0.1" x1="1535.5" y1="2.18557e-08" x2="1535.5" y2="9039" stroke="#979797"/>
+            </svg>
         </div>
     </div>
 </div>
