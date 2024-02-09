2af5972cf5915c790d38525af2b7faeda446bc48
Wilfried <64591090+la2spaille@users.noreply.github.com>
2024-02-09 16:09:00 +0100

    Feat (ActuEvent) : Init

---
+++ dotstarter/layouts/actu-x-event/actu-x-event.php
@@ -0,0 +1,9 @@
+<div class="f-actu-x-event">
+    <div class="l-container">
+        <div class="f-actu-x-event__tb">
+            <div class="f-actu-x-event__wrapper">
+
+            </div>
+        </div>
+    </div>
+</div>
