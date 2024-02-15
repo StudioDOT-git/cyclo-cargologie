0f125142614674613f254b3f944a73bfe9b329f1
Wilfried <64591090+la2spaille@users.noreply.github.com>
2024-02-15 11:10:12 +0100

    Feat (generic-content) : setup acf

--- dotstarter/layouts/generic-content/generic-content.js
+++ dotstarter/layouts/generic-content/generic-content.js
@@ -1,49 +1,10 @@
-import gsap from 'gsap';
-import SplitText from "gsap/SplitText";
-import DrawSVGPlugin from "gsap/DrawSVGPlugin";
+import Slider from '../../../assets/js/components/slider.js'
 
-import {
-    gsapHeading1,
-    gsapParagraph,
-    gsapContentCards,
-    gsapLineDeco
-} from "../../../assets/js/animations/text.js";
-
-function genericContentAnims() {
-
-    const layoutClass = '.f-generic-content';
-    const layouts = document.querySelectorAll(layoutClass);
-
-    if (!layouts) return;
-    if (!document.querySelector(`.f-generic-content`)) return;
-
-    gsap.utils.toArray(layoutClass).forEach(layout => {
-
-        let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: layout.querySelector(`${layoutClass} .l-container`),
-                start: "top 80%",
-                toggleActions: "play none play none"
-                // markers: true
-            }
-        });
-
-        let delay = 0; // initial delay value, incremented for each element
-
-        if (layout.querySelectorAll(`${layoutClass}__main .a-fade-in`)) {
-
-            let dropdownslistItems = layout.querySelectorAll(`${layoutClass}__main .a-fade-in`);
-
-            dropdownslistItems.forEach((dropdownslistItem) => {
-                tl.add(function () {
-                    dropdownslistItem.classList.add("is-inview");
-                }, delay);
-                delay += 0.15;
-            });
-        }
-    })
-}
-
-export {
-    genericContentAnims
-}
+const GenericContentSlider = new Slider('f-generic-content', {
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  mobileFirst: true,
+  draggable: true,
+  dots: true
+})
+export default GenericContentSlider
--- dotstarter/layouts/generic-content/generic-content.php
+++ dotstarter/layouts/generic-content/generic-content.php
@@ -6,17 +6,20 @@ $bg_color = get_sub_field('bg_color');
 $show_top_border = get_sub_field('show_top_border');
 ?>
 
-<div class="f-generic-content f-layout <?= $show_top_border ? 'f-layout--has-border-top' : '' ?> <?= $bg_color ? 'bg-' . $bg_color : '' ?>">
+<div
+    id="f-generic-content"
+    class="f-generic-content f-layout <?= $show_top_border ? 'f-layout--has-border-top' : '' ?> <?= $bg_color ? 'bg-' . $bg_color : '' ?>">
     <div class="l-container">
-        <div class="f-generic-content__main">
-            <?php if (have_rows('content')) : ?>
-                <?php while (have_rows('content')) :
-                    the_row(); ?>
-                    <?php switch (get_row_layout()):
+        <div class="f-generic-content__tb">
+            <div class="f-generic-content__main">
+                <?php if (have_rows('content')) : ?>
+                    <?php while (have_rows('content')) :
+                        the_row(); ?>
+                        <?php switch (get_row_layout()):
                         case 'title':
                             $type = get_sub_field('type');
                             $size = get_sub_field('size');
-                            $tag_open = "<$type class=\"generic-title $size a-fade-in\">";
+                            $tag_open = "<$type class=\"generic-title $size \">";
                             $tag_close = "</$type>";
 
                             $full_title = $tag_open;
@@ -26,28 +29,57 @@ $show_top_border = get_sub_field('show_top_border');
                             echo $full_title;
                             break;
                         case 'text': ?>
-                            <div class="generic-content-text wysiwyg a-fade-in">
+                            <div class="generic-content-text wysiwyg">
                                 <?php the_sub_field('text'); ?>
                             </div>
-                        <?php break;
+                            <?php break;
                         case 'text-en': ?>
-                            <div class="generic-content-text-en wysiwyg a-fade-in">
+                            <div class="generic-content-text-en wysiwyg">
                                 <?php the_sub_field('text_en'); ?>
                             </div>
-                        <?php break;
+                            <?php break;
                         case 'image':
                             $image = get_sub_field('image');
                             $is_large = get_sub_field('is_large');
-                        ?>
-                            <figure class="generic-image a-fade-in slide-ttb <?= $is_large ? 'is-large' : '' ?>">
-                                <?php echo wp_get_attachment_image($image['ID'], 'full'); ?>
+                            ?>
+                            <figure class="generic-image slide-ttb <?= $is_large ? 'is-large' : '' ?>">
+                                <?= wp_get_attachment_image($image['ID'], 'full'); ?>
                                 <figcaption>
-                                    <?php echo $image['caption']; ?>
+                                    <?= $image['caption']; ?>
                                 </figcaption>
                             </figure>
-                        <?php break;
+                            <?php break;
+                        case 'slider':
+                            $images = get_sub_field('gallery');
+                            $is_large = get_sub_field('is_large');
+                            ?>
+                            <div class="f-generic-content__slider-wrapper">
+                                <div
+                                    class="generic-gallery f-generic-content__slider <?= $is_large ? 'is-large' : '' ?>">
+                                    <?php foreach ($images as $image) : ?>
+                                        <figure class="generic-gallery__item f-generic-content__slide">
+                                            <?= wp_get_attachment_image($image['ID'], 'full'); ?>
+                                            <figcaption>
+                                                <?= $image['caption']; ?>
+                                            </figcaption>
+                                        </figure>
+                                    <?php endforeach; ?>
+
+                                </div>
+
+                                <div class="f-generic-content__index c-slider__index">
+                                    <span class="c-slider__prev btn-prev"><img
+                                            src="<?= DOT_THEME_URI ?>/assets/icons/slider-arrow-left.svg"/></span>
+                                    <div class="c-slider__index-mid">
+                                        <span class="current-index">1</span> / <span class="total-slides">0</span>
+                                    </div>
+                                    <span class="c-slider__next btn-next"><img src="<?= DOT_THEME_URI ?>/assets/icons/slider-arrow-right.svg"/></span>
+                                </div>
+                            </div>
+
+                            <?php break;
                         case 'columns': ?>
-                            <div class="generic-columns a-fade-in">
+                            <div class="generic-columns">
                                 <?php if (have_rows('columns')) :
                                     while (have_rows('columns')) :
                                         the_row(); ?>
@@ -56,7 +88,7 @@ $show_top_border = get_sub_field('show_top_border');
                                                 <?php
                                                 $title_size = get_sub_field('title_size');
                                                 $title = get_sub_field('title');
-                                                echo "<h3 class=\"generic-columns__title a-fade-in $title_size\">$title</h3>";
+                                                echo "<h3 class=\"generic-columns__title $title_size\">$title</h3>";
                                                 ?>
                                             <?php endif; ?>
                                             <?php if (get_sub_field('text')) : ?>
@@ -65,13 +97,13 @@ $show_top_border = get_sub_field('show_top_border');
                                                 </p>
                                             <?php endif; ?>
                                         </div>
-                                <?php endwhile;
+                                    <?php endwhile;
                                 endif; ?>
                             </div>
-                        <?php break;
+                            <?php break;
                         case 'embed': ?>
                             <?php $force_ratio = get_sub_field('force_ratio') ?>
-                            <div class="generic-embed <?= $force_ratio ? 'is-16-9' : '' ?> a-fade-in slide-ttb">
+                            <div class="generic-embed <?= $force_ratio ? 'is-16-9' : '' ?> slide-ttb">
                                 <div class="generic-embed__embed">
                                     <?php the_sub_field('embed') ?>
                                 </div>
@@ -79,7 +111,7 @@ $show_top_border = get_sub_field('show_top_border');
                             <?php break;
                         case 'buttons':
                             if (have_rows('button_repeater')) : ?>
-                                <div class="f-generic-content__buttons a-fade-in">
+                                <div class="f-generic-content__buttons">
                                     <?php while (have_rows('button_repeater')) :
                                         the_row();
                                         while (have_rows('button')) :
@@ -88,17 +120,18 @@ $show_top_border = get_sub_field('show_top_border');
                                         endwhile;
                                     endwhile; ?>
                                 </div>
-            <?php endif;
+                            <?php endif;
                     endswitch;
-                endwhile;
-            endif; ?>
-        </div>
-        <?php if (get_sub_field('embed') && !is_admin()) : ?>
-            <div class="f-generic-content__sidebar a-fade-in">
-                <div class="f-generic-content__sidebar-content">
-                    <?php the_sub_field('embed'); ?>
-                </div>
+                    endwhile;
+                endif; ?>
             </div>
-        <?php endif; ?>
+            <?php if (get_sub_field('embed') && !is_admin()) : ?>
+                <div class="f-generic-content__sidebar">
+                    <div class="f-generic-content__sidebar-content">
+                        <?php the_sub_field('embed'); ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
