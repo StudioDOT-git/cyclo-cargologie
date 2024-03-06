fa9636ac82337f4374ca59854bed2bdd5e605afe
Pierrick <pierrick@studio-dot.fr>
2024-03-06 18:24:37 +0100

    feat(key-numbers): add acf+php+scss

---
+++ dotstarter/layouts/key-numbers/key-numbers.php
@@ -0,0 +1,29 @@
+<div class="f-key-numbers">
+
+    <div class="l-container">
+        <h1 class="f-key-numbers__title heading2">
+            <?php the_sub_field('title') ?>
+        </h1>
+        <div class="f-key-numbers__wrapper">
+            <?php while (have_rows('columns')):
+                the_row() ?>
+                <div class="f-key-numbers__item">
+                    <?php if (have_rows('sticker')): ?>
+                        <div class="f-key-numbers__section-deco">
+                            <?php while (have_rows('sticker')):
+                                the_row() ?>
+                                <?php dot_the_layout_part('deco') ?>
+                            <?php endwhile; ?>
+                        </div>
+                    <?php endif; ?>
+                    <h3 class="f-key-numbers__number">
+                        <?= get_sub_field('number') ?>
+                    </h3>
+                    <p class="f-key-numbers__description heading6">
+                        <?= get_sub_field('description') ?>
+                    </p>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+</div>
