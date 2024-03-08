13db9ee123b7cbbc85a054eb1a01fa1c8986488f
Pierrick <pierrick@studio-dot.fr>
2024-03-08 14:35:58 +0100

    feat(final adjustments): acf + php +scss

--- dotstarter/layouts/members-grid/members-grid.php
+++ dotstarter/layouts/members-grid/members-grid.php
@@ -1,28 +1,36 @@
 <div class="f-members-grid">
     <div class="l-container">
-        <div class="f-members-grid__title heading2">
-            <?= get_sub_field('title') ?>
+        <div class="f-members-grid__headings">
+            <?php if (have_rows('sticker')): ?>
+                <?php while (have_rows('sticker')):
+                    the_row() ?>
+                    <?php dot_the_layout_part('deco') ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+            <div class="f-members-grid__title heading2">
+                <?= get_sub_field('title') ?>
+            </div>
+            <p class="f-members-grid__description body-lg">
+                <?= get_sub_field('description') ?>
+            </p>
         </div>
         <div class="f-members-grid__list">
             <?php if (have_rows('members')): ?>
                 <?php while (have_rows('members')):
                     the_row(); ?>
                     <div class="f-members-grid__item">
-                        <div class="f-members-grid__item__img">
+                        <div class="f-members-grid__item-image">
                             <img src="<?= get_sub_field('image')['url'] ?>" alt="<?= get_sub_field('image')['alt'] ?>">
                         </div>
-                        <div class="f-members-grid__item__name">
+                        <div class="f-members-grid__item-name heading5">
                             <?= get_sub_field('name') ?>
                         </div>
-                        <div class="f-members-grid__item__position">
+                        <div class="f-members-grid__item-position heading7">
                             <?= get_sub_field('role') ?>
                         </div>
-                        <div class="f-members-grid__item__description">
+                        <div class="f-members-grid__item-description">
                             <?= get_sub_field('description') ?>
                         </div>
-                        <!-- <?= dot_get_icon('linkedin', 'f-members-grid__item__linkedin') ?>
-                            <?= dot_get_icon('twitter', 'f-members-grid__item__linkedin') ?> -->
-
                     </div>
                 <?php endwhile; ?>
             <?php endif; ?>
