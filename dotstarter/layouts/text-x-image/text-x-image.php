c8415e9e4c0c22e4b4f0caa8802c97144aaf6206
Pierrick <pierrick@studio-dot.fr>
2024-03-12 18:06:30 +0100

    fix(text-x-image): fix sticker

--- dotstarter/layouts/text-x-image/text-x-image.php
+++ dotstarter/layouts/text-x-image/text-x-image.php
@@ -16,8 +16,7 @@
                         <?php if ($image_wrapper['image_deco']['is_active']): ?>
                             <div class="f-text-x-image__image-deco">
                                 <div class="c-deco">
-                                    <img
-                                        src="<?= DOT_THEME_URI . '/assets/img/deco/' . $image_wrapper['image_deco']['deco'] . '.png' ?>"
+                                    <img src="<?= DOT_THEME_URI . '/assets/img/deco/' . $image_wrapper['image_deco']['deco'] . '.svg' ?>"
                                         alt="Décoration">
                                 </div>
                             </div>
@@ -32,9 +31,15 @@
                                 <?php endwhile; ?>
                             </div>
                         <?php endif; ?>
-                        <h6 class="f-text-x-image__subtitle"><?= get_sub_field('subtitle') ?></h6>
-                        <h3 class="f-text-x-image__title heading2"><?= get_sub_field('title') ?></h3>
-                        <div class="f-text-x-image__paragraph body-lg"><?= get_sub_field('text') ?></div>
+                        <h6 class="f-text-x-image__subtitle">
+                            <?= get_sub_field('subtitle') ?>
+                        </h6>
+                        <h3 class="f-text-x-image__title heading2">
+                            <?= get_sub_field('title') ?>
+                        </h3>
+                        <div class="f-text-x-image__paragraph body-lg">
+                            <?= get_sub_field('text') ?>
+                        </div>
                         <?php while (have_rows('button')):
                             the_row() ?>
                             <?php dot_the_layout_part('button') ?>
@@ -45,12 +50,12 @@
         </div>
         <div class="f-text-x-image__bg">
             <svg viewBox="0 0 1538 4930" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line opacity="0.1" x1="0.5" y1="2.18557e-08" x2="0.499605" y2="9039" stroke="#979797"/>
-                <line opacity="0.1" x1="307.5" y1="2.18557e-08" x2="307.5" y2="9039" stroke="#979797"/>
-                <line opacity="0.1" x1="614.5" y1="2.68598e-08" x2="614.5" y2="9039" stroke="#979797"/>
-                <line opacity="0.1" x1="921.5" y1="2.68598e-08" x2="921.5" y2="9039" stroke="#979797"/>
-                <line opacity="0.1" x1="1228.5" y1="2.18557e-08" x2="1228.5" y2="9039" stroke="#979797"/>
-                <line opacity="0.1" x1="1535.5" y1="2.18557e-08" x2="1535.5" y2="9039" stroke="#979797"/>
+                <line opacity="0.1" x1="0.5" y1="2.18557e-08" x2="0.499605" y2="9039" stroke="#979797" />
+                <line opacity="0.1" x1="307.5" y1="2.18557e-08" x2="307.5" y2="9039" stroke="#979797" />
+                <line opacity="0.1" x1="614.5" y1="2.68598e-08" x2="614.5" y2="9039" stroke="#979797" />
+                <line opacity="0.1" x1="921.5" y1="2.68598e-08" x2="921.5" y2="9039" stroke="#979797" />
+                <line opacity="0.1" x1="1228.5" y1="2.18557e-08" x2="1228.5" y2="9039" stroke="#979797" />
+                <line opacity="0.1" x1="1535.5" y1="2.18557e-08" x2="1535.5" y2="9039" stroke="#979797" />
             </svg>
         </div>
     </div>
