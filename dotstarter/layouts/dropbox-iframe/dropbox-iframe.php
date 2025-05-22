337b66487d623df34a4c6ac816af8bdf10a25c65
Pierrick <dev@studio-dot.fr>
2025-05-22 16:16:38 +0200

    Adds Dropbox Iframe layout
    Adds a new layout for embedding Dropbox iframes.
    
    This layout includes fields for:
    - A sticker (using a clone of an existing group)
    - Subtitle, Title, and Description
    - Iframe shortcode
    
    The layout renders the iframe within a container, allowing users to easily embed Dropbox content into pages.

---
+++ dotstarter/layouts/dropbox-iframe/dropbox-iframe.php
@@ -0,0 +1,32 @@
+<div class="f-dropbox-iframe l-layout">
+    <div class="l-container">
+        <div class="l-layout__headings <?php if (have_rows('sticker')): ?>l-layout__headings--with-deco<?php endif; ?>">
+            <div class="l-layout__deco-container">
+                <?php if (have_rows('sticker')): ?>
+                    <?php while (have_rows('sticker')):
+                        the_row() ?>
+                        <?php dot_the_layout_part('deco') ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
+            <div class="l-layout__titles">
+                <h6 class="l-layout__subtitle">
+                    <?= get_sub_field('subtitle') ?>
+                </h6>
+                <h2 class="l-layout__title">
+                    <?= get_sub_field('title') ?>
+                </h2>
+            </div>
+            <div class="l-layout__description body-md">
+                <?= get_sub_field('description') ?>
+            </div>
+        </div>
+
+        <div class="f-dropbox-iframe__iframe-container">
+            <?php
+            $$iframe_shortcode = get_sub_field('iframe_shortcode');
+            echo do_shortcode($$iframe_shortcode);
+            ?>
+        </div>
+    </div>
+</div>
