168754c13fb6aaf9d43d7bda914ed7cf5f517fe6
Pierrick <dev@studio-dot.fr>
2025-09-11 11:22:47 +0200

    Improves media slider on single media page
    Enhances the media slider component on the single media page for better responsiveness and visual consistency.
    
    - Enables adaptive height for the slider.
    - Adjusts CSS to ensure proper rendering in Firefox.
    - Modifies image sizing and container behavior for improved layout on different screen sizes.

--- assets/js/templates/bibliotheque-media-single.js
+++ assets/js/templates/bibliotheque-media-single.js
@@ -3,9 +3,14 @@ import Slider from '../../../assets/js/components/slider.js'
 const singleBibliothequeMediaSlider = new Slider('t-media-single-slider', {
   slidesToScroll: 1,
   slidesToShow: 1,
-  mobileFirst: true,
+  // mobileFirst: true,
   draggable: true,
+  adaptiveHeight: true,
   dots: true
+  // cssEase: 'ease',
+  // speed: 400,
+  // centerMode: false,
+  // variableWidth: false
 })
 
 export default singleBibliothequeMediaSlider
